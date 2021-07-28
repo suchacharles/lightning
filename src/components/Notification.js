906e28d34c455eeabee880150998a925d52e65b3
CharlesCarrasco <74469148+CharlesCarrasco@users.noreply.github.com>
2021-07-27 18:56:45 -0700

    Update Notification.js

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -3,7 +3,7 @@ export default {
     template: `
 <div class="lightningNotification">
 
-<div class="text-white px-6 py-4 border-0 rounded absolute top-0 left-48 shadow-xl bg-indigo-500 mx-8 my-4" onclick="closeNotification()" id="notification" style="display: none;">
+<div class="text-white px-6 py-4 mt-8 border-0 rounded absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 shadow-xl bg-indigo-500 my-4" onclick="closeNotification()" id="notification" style="display: none;">
   <span class="text-xl inline-block mr-5 align-middle">
     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
       <path fill-rule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z" clip-rule="evenodd" />
